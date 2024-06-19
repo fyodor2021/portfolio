4cd49aa13a7cfd5619bdc8c8788ce7bef9e066a9
Vedoor Barakat <vedoorbbs@gmail.com>
2024-06-18 22:55:28 -0400

    uncommenting contact form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,54 +119,54 @@
     </section><!--/.portfolio-->
 
     <section id="contact" class="contact">
-{{--        <div class="section-heading text-center">--}}
-{{--            <h2>contact me</h2>--}}
-{{--        </div>--}}
-{{--        <div class="container">--}}
-{{--            <div class="contact-content">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-md-offset-1 col-md-5 col-sm-6">--}}
-{{--                        <div class="single-contact-box">--}}
-{{--                            <div class="contact-form">--}}
-{{--                                <form method="POST" action="/contact">--}}
-{{--                                    @csrf--}}
-{{--                                    <div class="row">--}}
-{{--                                        <div class="col-sm-6 col-xs-12">--}}
-{{--                                            <div class="form-group">--}}
-{{--                                                <input type="text" class="form-control" id="name" placeholder="Name*" name="name">--}}
-{{--                                            </div><!--/.form-group-->--}}
-{{--                                        </div><!--/.col-->--}}
-{{--                                        <div class="col-sm-6 col-xs-12">--}}
-{{--                                            <div class="form-group">--}}
-{{--                                                <input type="email" class="form-control" id="email" placeholder="Email*" name="email">--}}
-{{--                                            </div><!--/.form-group-->--}}
-{{--                                        </div><!--/.col-->--}}
-{{--                                    </div><!--/.row-->--}}
-{{--                                    <div class="row">--}}
-{{--                                        <div class="col-sm-12">--}}
-{{--                                            <div class="form-group">--}}
-{{--                                                <input type="text" class="form-control" id="subject" placeholder="Subject" name="subject">--}}
-{{--                                            </div><!--/.form-group-->--}}
-{{--                                        </div><!--/.col-->--}}
-{{--                                    </div><!--/.row-->--}}
-{{--                                    <div class="row">--}}
-{{--                                        <div class="col-sm-12">--}}
-{{--                                            <div class="form-group">--}}
-{{--                                                <textarea class="form-control" rows="8" id="comment" name="comment" placeholder="Message" ></textarea>--}}
-{{--                                            </div><!--/.form-group-->--}}
-{{--                                        </div><!--/.col-->--}}
-{{--                                    </div><!--/.row-->--}}
-{{--                                    <div class="row">--}}
-{{--                                        <div class="col-sm-12">--}}
-{{--                                            <div class="single-contact-btn">--}}
-{{--                                                <button class="contact-btn" type="submit">submit</button>--}}
-{{--                                            </div><!--/.single-single-contact-btn-->--}}
-{{--                                        </div><!--/.col-->--}}
-{{--                                    </div><!--/.row-->--}}
-{{--                                </form><!--/form-->--}}
-{{--                            </div><!--/.contact-form-->--}}
-{{--                        </div><!--/.single-contact-box-->--}}
-{{--                    </div><!--/.col-->--}}
+        <div class="section-heading text-center">
+            <h2>contact me</h2>
+        </div>
+        <div class="container">
+            <div class="contact-content">
+                <div class="row">
+                    <div class="col-md-offset-1 col-md-5 col-sm-6">
+                        <div class="single-contact-box">
+                            <div class="contact-form">
+                                <form method="POST" action="/contact">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-sm-6 col-xs-12">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" id="name" placeholder="Name*" name="name">
+                                            </div><!--/.form-group-->
+                                        </div><!--/.col-->
+                                        <div class="col-sm-6 col-xs-12">
+                                            <div class="form-group">
+                                                <input type="email" class="form-control" id="email" placeholder="Email*" name="email">
+                                            </div><!--/.form-group-->
+                                        </div><!--/.col-->
+                                    </div><!--/.row-->
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" id="subject" placeholder="Subject" name="subject">
+                                            </div><!--/.form-group-->
+                                        </div><!--/.col-->
+                                    </div><!--/.row-->
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <textarea class="form-control" rows="8" id="comment" name="comment" placeholder="Message" ></textarea>
+                                            </div><!--/.form-group-->
+                                        </div><!--/.col-->
+                                    </div><!--/.row-->
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="single-contact-btn">
+                                                <button class="contact-btn" type="submit">submit</button>
+                                            </div><!--/.single-single-contact-btn-->
+                                        </div><!--/.col-->
+                                    </div><!--/.row-->
+                                </form><!--/form-->
+                            </div><!--/.contact-form-->
+                        </div><!--/.single-contact-box-->
+                    </div><!--/.col-->
                     <div class="col-md-offset-1 col-md-5 col-sm-6">
                         <div class="single-contact-box">
                             <div class="contact-adress">
@@ -188,9 +188,9 @@
 
                         </div><!--/.single-contact-box-->
                     </div><!--/.col-->
-{{--                </div><!--/.row-->--}}
-{{--            </div><!--/.contact-content-->--}}
-{{--        </div><!--/.container-->--}}
+                </div><!--/.row-->
+            </div><!--/.contact-content-->
+        </div><!--/.container-->
 
     </section><!--/.contact-->
 
